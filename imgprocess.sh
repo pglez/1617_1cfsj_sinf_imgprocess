cd06e606ae245e466ea474a340a15a04b1333ac5
pglez <pglez@ieselcaminas.org>
2017-04-04 10:33:28 +0200

    My happy first commit

--- imgprocess.sh
+++ imgprocess.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Script to convert all image jpeg files from a directory (argument) into png format file
-# and it also reduces size of images 
+# and it also reduces size of images
 
 # Sanity checks ?
 # ...
@@ -10,12 +10,12 @@
 
 ls -1 $1*.jpg > .imagefiles.tmp
 
-while read imgfile 
+while read imgfile
 do
 	lengthname=${#imgfile}
-	namefile=${imgfile;0;($lengthname-4)}
-	convert $imgfile -resize 200x200 $namefile.png	
+	namefile=${imgfile:0:($lengthname-4)}
+	convert $imgfile -resize 200x200 $namefile.png
 
 done < .imagefiles.tmp
-rm imagefiles.tmp
+rm .imagefiles.tmp
 exit 0
