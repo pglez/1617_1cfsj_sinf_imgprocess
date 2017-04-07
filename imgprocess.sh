85209f76376ec4954b0f1ec4590ba248be73cb69
valkyriaivan <valkyriaivan@yahoo.es>
2017-04-07 11:41:36 +0200

    HOla

--- imgprocess.sh
+++ imgprocess.sh
@@ -1,21 +1,39 @@
 #!/bin/bash
 
 # Script to convert all image jpeg files from a directory (argument) into png format file
-# and it also reduces size of images 
+# and it also reduces size of images
 
 # Sanity checks ?
 # ...
 
 # Main code
 
-ls -1 $1*.jpg > .imagefiles.tmp
 
-while read imgfile 
+ls -1 $1* > .imagefiles.tmp
+
+option=2
+force=0
+input=500x500
+num=1
+
+lengthname=${#imgfile}
+namefile=${imgfile:0:($lengthname-4)}
+namefile+="$num"
+
+while read imgfile
 do
-	lengthname=${#imgfile}
-	namefile=${imgfile;0;($lengthname-4)}
-	convert $imgfile -resize 200x200 $namefile.png	
-
+	case $option in
+		1)
+		convert $imgfile -resize $input $namefile.png
+		;;
+		2)
+		if [$force=0]; then
+			input+="!"
+			convert $imgfile -resize $input $namefile.png
+		else
+			convert $imgfile -resize $input $namefile.png
+		fi
+	esac
 done < .imagefiles.tmp
-rm imagefiles.tmp
+rm .imagefiles.tmp
 exit 0
