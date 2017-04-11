f340a85ca16319c10acc9c8f233833cfb742531c
DiegoTG <diegotg1997@gmail.com>
2017-04-11 10:37:43 +0200

    Image edit

--- imgprocess.sh
+++ imgprocess.sh
@@ -1,22 +1,16 @@
-function extract_name () {
-	filename=$(basename "$imgfile")
-	path=$(dirname "$imgfile")
-	namefile="${filename%.*}"
-	pathfile="$path/$namefile"
+
+function image-edit () {
+	ls -1 $load*.* > .imagefiles.tmp
+	format="png"
+
+	while read imgfile
+	do
+		extract_name
+
+		convert $imgfile -auto-level $pathfile.$format
+
+
+	done < .imagefiles.tmp
+	rm .imagefiles.tmp
 }
 
-load="/home/INFORMATICA/alu20490173w/1617_1cfsj_sinf_imgprocess-master/images/"
-ls -1 $load*.* > .imagefiles.tmp
-
-format="png"
-
-while read imgfile
-do
-	extract_name
-
-	convert $imgfile -auto-level $pathfile.$format
-
-
-done < .imagefiles.tmp
-rm .imagefiles.tmp
-exit 0
