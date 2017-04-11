07aa267201ab1b293f4b5b5cd7b565cea66fb306
prim-lidia <lidiamatinvelasco@gmail.com>
2017-04-11 10:09:04 +0200

    define_path, overwrite_load, extract_name functions

--- imgprocess.sh
+++ imgprocess.sh
@@ -1,21 +1,41 @@
 #!/bin/bash
 
-# Script to convert all image jpeg files from a directory (argument) into png format file
-# and it also reduces size of images 
+#Add code to save in new folder
+function define_path () {
+  if [[ ! $path = "" ]]
+  then
+    path=$load
+  fi
+}
+#function to change load if there are more than one processing option.
+#Use function overwrite_load in all options
+function overwrite_load () {
+  if [[ counter = 1 ]]
+  then
+    load=$path
+  fi
+}
+#function to define final_file (path)
+function extract_name () {
+  filename=$(basename "$imgfile") #Extract the name file ex: file.jpg
+  namefile="${filename%.*}" #Extract name of a file without extension
+  pathfile="$path/$namefile" #Put together path and name file, then we choose the extension to convert
+}
 
-# Sanity checks ?
-# ...
+counter=0
 
-# Main code
+define_path #function
 
-ls -1 $1*.jpg > .imagefiles.tmp
+echo "Format:"
+read format
 
-while read imgfile 
+ls -1 $load*.* > .imagefiles.tmp
+while read imgfile
 do
-	lengthname=${#imgfile}
-	namefile=${imgfile;0;($lengthname-4)}
-	convert $imgfile -resize 200x200 $namefile.png	
+	extract_name
+	convert $imgfile -resize 200x200 $pathfile.$format
 
 done < .imagefiles.tmp
-rm imagefiles.tmp
+rm .imagefiles.tmp
+
 exit 0
